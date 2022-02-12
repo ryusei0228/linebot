2618d260d878d00900ce3158d33e0954a8df4e1f
ryusei0228 <98815749+ryusei0228@users.noreply.github.com>
2022-02-12 13:45:39 +0900

    Update app_line.py

--- mylib/app_line.py
+++ mylib/app_line.py
@@ -1,18 +1,35 @@
 import torch
 from Config import Config
 from tokenizer import Tokenizer
-import EncoderDecoder as E_D
+from EncoderDecoder import build_model
 from EncoderDecoder import EncoderDecoder
-import evaluate as ev
+from evaluate import evaluate
+import requests
 
 def prep():
+    
+    id = '1DubBO3fagYccVuKwrIVa4zGvsCzW7FCr'
+    destination = ".data/ckpt.pth"
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)
+    
     device = torch.device("cpu")
 
     state_dict = torch.load(f'{Config.data_dir}/{Config.fn}.pth', map_location=device)
 
     tokenizer = Tokenizer.from_pretrained(Config.model_name)
 
-    model = E_D.build_model(Config).to(device)
+    model = build_model(Config).to(device)
     model.load_state_dict(state_dict["model"])
     model.eval()
     model.freeze()
@@ -20,8 +37,23 @@ def prep():
 def line(message):
     s = message
     print("BOT>", end = "")
-    text = ev.evaluate(Config, s, tokenizer, model, device)
+    text = evaluate(Config, s, tokenizer, model, device)
     print(text)
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
 
 
 
