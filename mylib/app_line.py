d74d34930b27e41c77a73de051be7872c0be5f63
ryusei0228 <98815749+ryusei0228@users.noreply.github.com>
2022-02-12 17:13:41 +0900

    Update app_line.py

--- mylib/app_line.py
+++ mylib/app_line.py
@@ -7,25 +7,10 @@ from evaluate import evaluate
 import requests
 
 def prep():
-    
-    id = '1DubBO3fagYccVuKwrIVa4zGvsCzW7FCr'
-    destination = "ckpt.pth"
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)
-    
+   
     device = torch.device("cpu")
 
-    state_dict = torch.load(f'{Config.fn}.pth', map_location=device)
+    state_dict = torch.load(f'https://github.com/ryusei0228/line/ckpt.pth', map_location=device)
 
     tokenizer = Tokenizer.from_pretrained(Config.model_name)
 
@@ -40,20 +25,6 @@ def line(message):
     text = evaluate(Config, s, tokenizer, model, device)
     print(text)
 
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
 
 
 
